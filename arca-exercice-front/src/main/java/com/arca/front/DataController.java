e3dbb4869be0e6720674546589942a605c2d08bf
Wifsimster <wifsimster@gmail.com>
2015-03-31 23:16:09 +0200

    Improve pagination and add binding with launch batch.

--- arca-exercice-front/src/main/java/com/arca/front/DataController.java
+++ arca-exercice-front/src/main/java/com/arca/front/DataController.java
@@ -1,11 +1,14 @@
 package com.arca.front;
 
+import com.arca.batch.Extractor;
 import com.arca.core.entity.DataEntity;
 import com.arca.core.manager.DataManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -17,21 +20,36 @@ public class DataController {
     // Slf4j logger
     private final static Logger LOGGER = LoggerFactory.getLogger(DataController.class);
 
-    @RequestMapping(value = "/data?page={page}&count={count}", method = RequestMethod.GET)
-    public List<DataEntity> getData(@PathVariable int page, @PathVariable int count) {
+    @RequestMapping(value = "/extract", method = RequestMethod.POST)
+    public void extractFile() {
+        Extractor extractor = new Extractor();
+        extractor.extractFile();
+    }
 
-        LOGGER.info("Page {}, Count {}", page, count);
-        
-        List<DataEntity> allData = new ArrayList<>();
+    @RequestMapping(value = "/data?page={page}&count={count}", method = RequestMethod.GET)
+    public DataList getData(@PathVariable int page, @PathVariable int count) {
+
+        DataList dataList = new DataList();
+        List<DataEntity> dataPaginate = new ArrayList<>();
 
         try {
-            allData = DataManager.getAllData(count * page, count);
-            LOGGER.info("Display data size : {}", allData.size());
+            // Get paginate data
+            dataPaginate = DataManager.getAllData(count * page, count);
+            long allDataSize = DataManager.getCount();
+
+            // Build response object
+            dataList.setCount(count);
+            dataList.setPage(page);
+            dataList.setPages((int) (DataManager.getCount() / count));
+            dataList.setSize((int) allDataSize);
+            dataList.setDataEntities(dataPaginate);
+            dataList.setSortBy("timestamp");
+            dataList.setSortOrder("asc");
         } catch (UnknownHostException e) {
             LOGGER.error("Error : {}", e);
         }
 
-        return allData;
+        return dataList;
     }
 
 }
