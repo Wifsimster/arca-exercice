e1783e370c9d34c5a59c5d9999acd704d4bf48b1
Wifsimster <wifsimster@gmail.com>
2015-04-06 18:12:23 +0200

    Add BatchTest test.

---
+++ arca-exercice-front/src/test/java/com/arca/front/BatchTest.java
@@ -0,0 +1,13 @@
+package com.arca.front;
+
+
+//@ContextConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+public class BatchTest {
+
+    // fyi : http://docs.spring.io/spring-batch/trunk/reference/html/testing.html
+    // Test start job
+    // Test stop job
+    // Test getting job info
+    // Test job resume with data coherence
+}
